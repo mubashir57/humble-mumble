0a024f14ceb9eace9c4636f58b0b5c4f68cf4e20
devLoop57 <mubashirirshad57@gmail.com>
2022-07-05 14:52:15 +0800

    first commit

---
+++ Assets/PlayerScorer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScorer : MonoBehaviour
+{
+    // Start is called before the first frame update
+   int hits=0;
+   private void OnCollisionEnter(Collision other) 
+   {
+        
+        Debug.Log("Total Hits: "+hits);
+         Debug.Log("reahn");
+         Debug.Log("reahn");
+
+        hits++;
+   }
+}
